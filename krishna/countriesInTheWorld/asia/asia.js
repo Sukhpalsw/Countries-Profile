ad7e813b5fc0ce15675cbb9d5ff6cf129de5e40f
Sukhpalsw <matrix7038@gmail.com>
2026-07-01 14:44:24 +0200

    Update the new changes in countries profiles

---
+++ krishna/countriesInTheWorld/asia/asia.js
@@ -0,0 +1,23 @@
+function loadHTML(id, url) {
+    fetch(url)
+        .then(response => {
+            if (!response.ok) throw new Error('Network response was not ok');
+            return response.text();
+        })
+        .then(data => {
+            // Fix image paths in the HTML content before inserting it
+            if (id === 'footer-placeholder') {
+                data = data.replace(/src="\.\/images\//g, 'src="../images/');
+            }
+            
+            document.getElementById(id).innerHTML = data;
+        })
+        .catch(error => {
+            document.getElementById(id).innerHTML = "<p style='color:red;'>Could not load " + url + "</p>";
+            console.error('Error loading', url, error);
+        });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    loadHTML('footer-placeholder', '../footer/footer.html');
+});
